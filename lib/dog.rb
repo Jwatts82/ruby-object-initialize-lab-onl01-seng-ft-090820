3003a76d784c3afd928c0c819380e91367756116
Jessica Watts <jessperr182@hotmail.com>
2020-09-15 20:23:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,6 @@
 class Dog
-  def initialize(breed)
+  def initialize(name, breed="Mutt")
+    @name = name
     @breed = breed
-  end
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
   end
 end
-
